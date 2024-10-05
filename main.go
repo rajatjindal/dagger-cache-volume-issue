5e1f52f986542f2d7fee00c72f7d2d683d1da686
Rajat Jindal <rajatjindal83@gmail.com>
2024-10-05 18:20:17 +0530

    with new file error
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- main.go
+++ main.go
@@ -4,33 +4,28 @@ import (
 	"context"
 	"dagger/foo/internal/dagger"
 	"fmt"
-	"strings"
 )
 
 type Foo struct{}
 
-func (f *Foo) TestCacheVolumePersistence(ctx context.Context) error {
-	_, err := f.PopulateCache(ctx)
+func (f *Foo) TestCacheVolumePersistence(ctx context.Context, input string) (string, error) {
+	_, err := f.PopulateCache(ctx, input)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	output, err := f.ListCache(ctx)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	if !strings.Contains(output, "bar.txt") {
-		return fmt.Errorf("%#v does not contain bar.txt file", output)
-	}
-
-	return nil
+	return output, nil
 }
 
-func (f *Foo) PopulateCache(ctx context.Context) (*dagger.Container, error) {
+func (f *Foo) PopulateCache(ctx context.Context, input string) (*dagger.Container, error) {
 	return dag.Container().From("alpine:latest").
 		WithMountedCache("/foo-cache", dag.CacheVolume("foo-cache")).
-		WithExec([]string{"sh", "-c", "echo 'i am in cache 123456' > /foo-cache/bar.txt"}).Terminal().
+		WithExec([]string{"sh", "-c", fmt.Sprintf("echo '%s' > /foo-cache/bar.txt", input)}).
 		Sync(ctx)
 }
 
@@ -39,6 +34,15 @@ func (f *Foo) ListCache(ctx context.Context) (string, error) {
 		Container().
 		From("alpine:latest").
 		WithMountedCache("/foo-cache", dag.CacheVolume("foo-cache")).
-		WithExec([]string{"sh", "-c", "cat /foo-cache/bar.txt"}).Terminal().
+		WithExec([]string{"sh", "-c", "cat /foo-cache/bar.txt"}).
 		Stdout(ctx)
+}
+
+func (f *Foo) WithNewFile(ctx context.Context) (*dagger.Container, error) {
+	return dag.
+		Container().
+		From("alpine:latest").
+		WithMountedCache("/foo-cache", dag.CacheVolume("foo-cache")).
+		WithNewFile("/foo-cache/somefile.txt", "contents of somefile").
+		Sync(ctx)
 }
